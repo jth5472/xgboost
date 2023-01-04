9462239895c368ca25afcc5f825ee4b0ffd811b9
jth5472 <JamesHenrich24@gmail.com>
2023-01-04 09:00:00 -0500

    remove CfsCpuCount check for linux

--- src/common/threading_utils.cc
+++ src/common/threading_utils.cc
@@ -26,33 +26,7 @@ namespace xgboost::common {
  */
 std::int32_t GetCGroupV1Count(std::filesystem::path const& quota_path,
                               std::filesystem::path const& peroid_path) {
-#if defined(__linux__)
-  // https://bugs.openjdk.java.net/browse/JDK-8146115
-  // http://hg.openjdk.java.net/jdk/hs/rev/7f22774a5f42
-  // RAM limit /sys/fs/cgroup/memory.limit_in_bytes
-  // swap limt /sys/fs/cgroup/memory.memsw.limit_in_bytes
-
-  auto read_int = [](char const* const file_path) noexcept {
-    std::ifstream fin(file_path);
-    if (!fin) {
-      return -1;
-    }
-    std::string value;
-    fin >> value;
-    try {
-      return std::stoi(value);
-    } catch (std::exception const&) {
-      return -1;
-    }
-  };
-  // complete fair scheduler from Linux
-  auto const cfs_quota(read_int(quota_path.c_str()));
-  auto const cfs_period(read_int(peroid_path.c_str()));
-  if ((cfs_quota > 0) && (cfs_period > 0)) {
-    return std::max(cfs_quota / cfs_period, 1);
-  }
-#endif  //  defined(__linux__)
-  return -1;
+ return -1;
 }
 
 std::int32_t GetCGroupV2Count(std::filesystem::path const& bandwidth_path) noexcept(true) {
